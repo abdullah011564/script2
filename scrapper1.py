f77f688cb7a169aabe12e570f776b807175c0da4
Abdullah m abdelsayed <93607930+abdullah011564@users.noreply.github.com>
2022-12-17 19:39:28 +0200

    Add files via upload

---
+++ scrapper1.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+gpu=input("Enter the name of the gpu: ")
+
+url='https://www.newegg.com/p/pl?SrchInDesc={gpu}&N=100007709&Order=1'
+
+result=requests.get(url)
+doc=BeautifulSoup(result.text,"html.parser")
+
+
+gpu_containers=doc.find_all(class_="item-cell")
+
+
+for gpu in gpu_containers:
+    try:
+      print("-" * 30)
+      print(gpu_containers.index(gpu)+1)
+      print(gpu.find(class_="item-info").find_all('a')[-2].string)
+      print("$",gpu.find(class_="price-current").strong.string)
+
+    except:
+        continue
